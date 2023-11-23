5379da3cefa2f3dbc2cc03c35a4c50b8a210a9a4
Darth Tayvius <collin.vosburgh@gmail.com>
2023-11-22 17:45:05 -0700

    implement basic navigation and add home and header components

--- menz-ui/src/app/app-routing.module.ts
+++ menz-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
     {
         path: 'menz-trip',
         loadChildren: () => import('./menz-trip/menz-trip.module').then(m => m.MenzTripModule)
@@ -9,6 +15,15 @@ const routes: Routes = [
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: "full"
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
     }
 ];
 
--- menz-ui/src/app/app.module.ts
+++ menz-ui/src/app/app.module.ts
@@ -5,12 +5,16 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         DashboardComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        HeaderComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
